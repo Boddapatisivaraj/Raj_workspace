edb49a1998b7e1ce697e6475843917f32cd41db9
Boddapatisivaraj <sboddapati280@gmail.com>
2023-01-10 12:11:39 +0530

    My Raj update

---
+++ raj/src/raj/ravi.java
@@ -0,0 +1,8 @@
+package raj;
+
+public class ravi {
+
+	public static void main(String args[]) {
+		System.out.println("sastry");
+	}
+}
---
+++ raj/src/screenshot/capturescreen.java
@@ -0,0 +1,36 @@
+package screenshot;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class capturescreen
+{
+	public static void main(String[] args) throws InterruptedException, IOException  
+	{
+		
+	WebDriver driver=WebDriverManager.chromedriver().avoidShutdownHook().create();
+    driver.get("https://www.instagram.com/");
+    driver.manage().window().maximize();
+    Thread.sleep(3000);
+
+    //Capturing screen and converting into file format
+    File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    //Creating folder under project
+    FileHandler.createDir(new File("Screens"));
+    //Copying file to local system
+    FileHandler.copy(src, new File("Screens\\image.png"));
+
+
+
+		
+
+	}
+}
